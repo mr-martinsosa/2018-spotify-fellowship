6a1418a74490fa7a73b5e2ff1ebd1b3dea782903
Martin Sosa <mr.martinsosa@gmail.com>
2018-06-14 23:16:42 -0400

    added question_two.rb

---
+++ question_two.rb
@@ -0,0 +1,37 @@
+def decode_string(s)
+    #initialize an empty variable to store the decoded string
+    decoded_string = ""
+
+    #initialize hash to organize numbers to characters better
+    hash = {}
+
+    #initialize array to store letters
+    letters = []
+
+    #initialize variable to hold current hash key
+    current_multiplier = 0
+
+    s.each_char do |letter|
+        if letter.to_i > 0 #if letter is greater than 0 it is an integer
+            hash.store(letter, letters) #store multiplier with empty array
+            current_multiplier = letter #set multiplier with current multiplier
+        elsif !(letter == "[" || letter == "]") #parse out anything not a letter
+            # p letter
+            hash[current_multiplier] = letters.push(letter) #append consecutive letters to array
+        else
+            letters = [] #empty array once it reaches ], should have no effect if [ 
+        end
+    end
+    reversed_hash = Hash[hash.to_a.reverse] #reverse hash to loop backwards
+    reversed_hash.each do |key, array|
+        array.reverse.each do |char| #reverse array to get original order of chars back 
+            decoded_string += char #append chars to decoded_string being built in loop
+        end
+        decoded_string = decoded_string.reverse #reverse string order if we were dealing with single element arrays
+        decoded_string *= key.to_i 
+    end
+    decoded_string
+end
+
+p decode_string("4[ab]") #"abababab"
+p decode_string("2[b3[a]]") #"baaabaaa"
